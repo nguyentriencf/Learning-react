c9d7db8cabbaaadd0ac3443d3ac7bb3290214d55
Nguyen Trien <trienvaj@gmail.com>
2021-07-28 20:14:41 +0700

    && operator

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,47 +1,23 @@
 import './App.css';
 import Employee from './Employee';
 
-const employee = [
-  {
-  firstName:"Trien",
-  lastName:"Nguyen Dinh Phat",
-  employeeAge:"21",
-  idEm:'1'
-  
-},
-  {
-  firstName:"Teo",
-  lastName:"Nguyen Van",
-  employeeAge:"31",
-  idEm:'2'
-},
-  {
-  firstName:"Bim",
-  lastName:"Lo Vi Song",
-  employeeAge:"32",
-  idEm:"3"
-}]
 
 function App() {
-  const baseEmployeeObject = {
-    role:'employee',
-    company:'CodeSandbox'
-  };
-  const firstEmployee= {...baseEmployeeObject,...employee[0]}
-  console.log(firstEmployee)
-  return (  
+  const employees =["Nguyen Dinh Phat Trien","Le Van Ti","Lo Vi Song"]
+  return ( 
     <div className="App">
-      <header className="App-header">
-      <h2>Welcom to my counter</h2>
-      {employee.map((employee) => {
-        // const {firstName, lastName, employeeAge} = employee;
-        return(
-          // <Employee firstName={firstName} lastName={lastName} employeeAge={employeeAge}/>
-          <Employee key={employee.idEm} {...employee}/>
-        )
-      })}
-      </header>
-    </div>
+      {employees && employees.length >0 && (
+        <div>
+          <h1>Em ployees List:</h1>
+          {employees.map((employee) => (
+            <h4>{employee}</h4>
+          ))}
+        </div>
+      )}
+
+
+    </div> 
+
   );
 }
 export default App;
