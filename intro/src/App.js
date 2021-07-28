2d93b76a872d38f0be91bf800816c9df65c1279e
Nguyen Trien <trienvaj@gmail.com>
2021-07-28 16:41:49 +0700

    first commit

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Employee from './Employee';
 
-const employeeInfo = [
+const employee = [
   {
   firstName:"Trien",
   lastName:"Nguyen Dinh Phat",
   employeeAge:"21",
   idEm:'1'
+  
 },
   {
   firstName:"Teo",
@@ -19,27 +20,28 @@ const employeeInfo = [
   lastName:"Lo Vi Song",
   employeeAge:"32",
   idEm:"3"
-}
-
-
-]
+}]
 
 function App() {
-  return (
+  const baseEmployeeObject = {
+    role:'employee',
+    company:'CodeSandbox'
+  };
+  const firstEmployee= {...baseEmployeeObject,...employee[0]}
+  console.log(firstEmployee)
+  return (  
     <div className="App">
       <header className="App-header">
       <h2>Welcom to my counter</h2>
-      {employeeInfo.map((employee) => {
+      {employee.map((employee) => {
         // const {firstName, lastName, employeeAge} = employee;
         return(
           // <Employee firstName={firstName} lastName={lastName} employeeAge={employeeAge}/>
           <Employee key={employee.idEm} {...employee}/>
         )
       })}
-      
       </header>
     </div>
   );
 }
-
 export default App;
